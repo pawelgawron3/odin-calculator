ea10733654d471f1bc964b0573221677f2d59523
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-21 15:22:50 +0200

    Create operate() function

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function add() {}
-function subtract() {}
-function multiply() {}
-function divide() {}
+function add(a, b) {}
+function subtract(a, b) {}
+function multiply(a, b) {}
+function divide(a, b) {}
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+    default:
+      console.log("Wrong operator!");
+  }
+}
