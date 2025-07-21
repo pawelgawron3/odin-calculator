d04e33392e76d4adb3a1c727a6c276e762218fee
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-21 16:53:18 +0200

    Update the program logic

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function add(a, b) {}
-function subtract(a, b) {}
-function multiply(a, b) {}
-function divide(a, b) {}
+let output = document.querySelector("#output");
+
+let operatorsDiv = document.querySelector("#operators-panel");
+let numbersDiv = document.querySelector("#numbers-panel");
+
+let equal = document.querySelector("#equal");
+let clear = document.querySelector("#clear");
+let point = document.querySelector("#point");
+
+let operators = [...operatorsDiv.querySelectorAll("button")];
+let numbers = [...numbersDiv.querySelectorAll("button")];
+
+function add(a, b) {
+  let result = parseFloat(a) + parseFloat(b);
+  output.textContent = result;
+}
+function subtract(a, b) {
+  let result = parseFloat(a) - parseFloat(b);
+  output.textContent = result;
+}
+function multiply(a, b) {
+  let result = parseFloat(a) * parseFloat(b);
+  output.textContent = result;
+}
+function divide(a, b) {
+  let result = parseFloat(a) / parseFloat(b);
+  output.textContent = result;
+}
 function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
@@ -20,3 +44,46 @@ function operate(operator, num1, num2) {
       console.log("Wrong operator!");
   }
 }
+
+function checkExpression(text) {
+  if (text.match(/[0-9]+[+\-*/][0-9]+/)) {
+    equal.click();
+  }
+}
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    let text = output.textContent;
+    checkExpression(text);
+    output.textContent += operator.textContent;
+  });
+});
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    output.textContent += number.textContent;
+  });
+});
+
+equal.addEventListener("click", () => {
+  let text = output.textContent;
+
+  let operatorMatch = text.match(/[+\-*/]/);
+  if (!operatorMatch) return;
+  let operator = operatorMatch[0];
+
+  let operatorIndex = text.indexOf(operator);
+
+  let operand1 = text.substring(0, operatorIndex).trim();
+  let operand2 = text.substring(operatorIndex + 1).trim();
+
+  operate(operator, operand1, operand2);
+});
+
+clear.addEventListener("click", () => {
+  output.textContent = "";
+});
+
+point.addEventListener("click", () => {
+  output.textContent += ".";
+});
